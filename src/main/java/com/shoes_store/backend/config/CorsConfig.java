cf18190cecad75aa26d68b6ee370abac8bd59f43
feriel <ferielbenhalima2@gmail.com>
2026-07-10 01:10:57 +0100

    fix CorsConfig

--- src/main/java/com/shoes_store/backend/config/CorsConfig.java
+++ src/main/java/com/shoes_store/backend/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
             "http://localhost:5173",
-            "http://localhost:3000"
+            "http://localhost:3000",
+            "https://*.vercel.app"
         ));
         config.setAllowedMethods(List.of(
             "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
